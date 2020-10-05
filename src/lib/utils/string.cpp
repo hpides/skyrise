0a8f5c0b8bb03953e1ad012624730d658d2adc8e
Tobias Maltenberger <tobias@maltenberger.com>
2020-10-05 18:25:16 +0200

    Add revised Aws::IOStream to Aws::String helper (#149)
    * initial
    
    * include aws-sdk-cpp
    
    * adjust libraries
    
    * ..
    
    * ...
    
    * Update .clang-format

--- src/lib/utils/string.cpp
+++ src/lib/utils/string.cpp
@@ -4,7 +4,10 @@
 
 #include "string.hpp"
 
+#include <memory>
 #include <string>
+
+#include <aws/core/Aws.h>
 
 namespace skyrise {
 
@@ -12,6 +15,14 @@ std::string TrimSourceFilePath(const std::string& path) {
   const auto src_position = path.find("/src/");
 
   return src_position == std::string::npos ? path : path.substr(src_position + 1);
+}
+
+Aws::String StreamToString(Aws::IOStream* stream) {
+  Aws::StringStream string_stream;
+  string_stream << stream->rdbuf();
+  stream->seekg(std::ios::beg);
+
+  return string_stream.str();
 }
 
 }  // namespace skyrise
--- src/lib/utils/string.hpp
+++ src/lib/utils/string.hpp
@@ -4,12 +4,18 @@
 
 #pragma once
 
+#include <memory>
 #include <string>
+
+#include <aws/core/Aws.h>
 
 namespace skyrise {
 
 // Crop a source file path to ensure readable assert messages (e.g., "/long/path/1234/src/lib/file.cpp" becomes
 // "src/lib/file.cpp")
 std::string TrimSourceFilePath(const std::string& path);
+
+// Convert a stream to a string
+Aws::String StreamToString(Aws::IOStream* stream);
 
 }  // namespace skyrise
