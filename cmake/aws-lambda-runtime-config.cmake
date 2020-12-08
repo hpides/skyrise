11d880779d4237be3cc64bf012bfb47b9891a22e
Jan Siebert <JanSiebert@users.noreply.github.com>
2020-12-08 14:05:27 +0100

    Link Lambda functions statically against AWS SDK (#228)

--- cmake/aws-lambda-runtime-config.cmake
+++ cmake/aws-lambda-runtime-config.cmake
@@ -3,7 +3,7 @@ include(CMakeFindDependencyMacro)
 find_dependency(CURL)
 
 set(AWS_LAMBDA_PACKAGING_SCRIPT ${PROJECT_SOURCE_DIR}/third_party/aws-lambda-cpp/packaging/packager)
-set(STRIP_FUNCTION_PACKAGE_SCRIPT ${PROJECT_SOURCE_DIR}/script/strip_function_package.sh)
+set(SKYRISE_STATIC_LAMBDA_PACKAGING_SCRIPT ${PROJECT_SOURCE_DIR}/script/static_function_packager.sh)
 
 function(aws_lambda_package_target target)
     find_program(LSB_RELEASE_EXEC lsb_release)
@@ -13,22 +13,31 @@ function(aws_lambda_package_target target)
     )
 
     if (${LSB_RELEASE_ID_SHORT} STREQUAL "Amazon")
-        set (PACKAGER_NO_LIBC "-d")
+
+        set(RUN_STRIP)
+        if (CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+            set(RUN_STRIP "-s")
+        endif()
 
         add_custom_target(
-            aws-lambda-package-${target}-stripped ALL
-            COMMAND ${STRIP_FUNCTION_PACKAGE_SCRIPT} ${target}.zip
-            DEPENDS aws-lambda-package-${target}
+            aws-lambda-package-${target} ALL
+            COMMAND ${SKYRISE_STATIC_LAMBDA_PACKAGING_SCRIPT} ${RUN_STRIP} $<TARGET_FILE:${target}>
+            DEPENDS ${target}
             WORKING_DIRECTORY ${CMAKE_PACKAGE_OUTPUT_DIRECTORY}
             JOB_POOL serial_jobs
         )
-    endif()
 
-    add_custom_target(
-        aws-lambda-package-${target} ALL
-        COMMAND ${AWS_LAMBDA_PACKAGING_SCRIPT} ${PACKAGER_NO_LIBC} $<TARGET_FILE:${target}>
-        DEPENDS ${target}
-        WORKING_DIRECTORY ${CMAKE_PACKAGE_OUTPUT_DIRECTORY}
-        JOB_POOL serial_jobs
-    )
+        unset(RUN_STRIP)
+    
+    else()
+
+        add_custom_target(
+            aws-lambda-package-${target} ALL
+            COMMAND ${AWS_LAMBDA_PACKAGING_SCRIPT} ${PACKAGER_NO_LIBC} $<TARGET_FILE:${target}>
+            DEPENDS ${target}
+            WORKING_DIRECTORY ${CMAKE_PACKAGE_OUTPUT_DIRECTORY}
+            JOB_POOL serial_jobs
+        )
+
+    endif()
 endfunction()
