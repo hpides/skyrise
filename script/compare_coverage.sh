4692136338ffff61b33597284e76ef7897f24e18
engelfa <39763370+engelfa@users.noreply.github.com>
2021-01-05 13:11:05 +0100

    Fix calculation (#292)

--- script/compare_coverage.sh
+++ script/compare_coverage.sh
@@ -6,7 +6,7 @@ commit_coverage=$(cat coverage_percentage.txt)
 master_coverage=$(curl --max-time 10 --silent --user ${JENKINS_HTTPS_AUTH} https://skyrise-ci.epic-hpi.de/job/skyrise/job/master/lastStableBuild/artifact/coverage_percentage.txt)
 
 if [ ${master_coverage} ]; then
-  if [ $(bc -l <<< "${commit_coverage%\%} >= ${master_coverage%\%}") ]; then
+  if [ $(bc -l <<< "${commit_coverage%\%} >= ${master_coverage%\%}") -eq 1 ]; then
     echo -n "SUCCESS"
   else
     echo -n "FAILURE"
