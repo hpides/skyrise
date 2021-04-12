8f2b5561947652065b6c267af61cd9b539a2d0e2
Thomas Bodner <thomas.bodner@hpi.de>
2021-04-12 13:11:11 +0200

    Introduce PascalCase for functions as in Google style guide (#418)
    * Introduce PascalCase for functions as in Google style guide
    
    * Fix funciton cases
    
    * Sort lines
    
    * Fix funciton cases
    
    * Fix funciton cases
    
    Co-authored-by: Julian Menzler <julianmenzler@gmail.com>

--- src/test/lib/utils/profiling/function_host_information_test.cpp
+++ src/test/lib/utils/profiling/function_host_information_test.cpp
@@ -7,105 +7,88 @@
 namespace skyrise {
 
 class FunctionHostInformationTest : public ::testing::Test {
- public:
-  FunctionHostInformationCollectorConfiguration config{"/tmp/exampleCgroupFile.txt",
-                                                       "echo \"5.6.7.8\"",
-                                                       false,
-                                                       "echo \"OSDetails\"",
-                                                       "ls",
-                                                       "tmp",
-                                                       "/tmp/exampleStatFile.txt",
-                                                       "/tmp/exampleUptimeFile",
-                                                       "/tmp/exampleCpuinfoFile",
-                                                       "/tmp/exampleMeminfoFile",
-                                                       true};
-
-  void set_up() const {
-    CreateFile("2:cpu,cpuacct:/sandbox-root-pQEzKi/sandbox-service-c53732/sandbox-f22810\n1:blkio:/\n",
-               config.cgroup_path);
-    CreateFile("ctxt 9999999\nbtime 123456\n", config.stat_path);
-    CreateFile("1234.56 789.10\n", config.uptime_path);
-    CreateFile(
-        "processor\t: 0\nmodel name\t: CpuModelName\nflags\t: three test flags\n\n"
-        "processor\t: 1\nmodel name\t: CpuModelName\nflags\t: three test flags\n",
-        config.cpuinfo_path);
-    CreateFile("MemTotal:      123456 kB\nMemFree:        7890 kB\n", config.meminfo_path);
-    mkdir(config.tmp_path.c_str(), 0777);
-    CreateFile("1234.56 789.10\n", "tmp/testTmpFile");
-  }
-
-  void tear_down() const {
-    remove("tmp/testTmpFile");
-    rmdir(config.tmp_path.c_str());
-    remove(config.meminfo_path.c_str());
-    remove(config.cpuinfo_path.c_str());
-    remove(config.uptime_path.c_str());
-    remove(config.stat_path.c_str());
-    remove(config.cgroup_path.c_str());
-  }
-
- private:
+ protected:
   static void CreateFile(const std::string& content, const std::string& filename) {
     std::ofstream file(filename);
     file << content;
   }
+
+  void SetUp() override {
+    CreateFile("2:cpu,cpuacct:/sandbox-root-pQEzKi/sandbox-service-c53732/sandbox-f22810\n1:blkio:/\n",
+               config_.cgroup_path);
+    CreateFile("ctxt 9999999\nbtime 123456\n", config_.stat_path);
+    CreateFile("1234.56 789.10\n", config_.uptime_path);
+    CreateFile(
+        "processor\t: 0\nmodel name\t: CpuModelName\nflags\t: three test flags\n\n"
+        "processor\t: 1\nmodel name\t: CpuModelName\nflags\t: three test flags\n",
+        config_.cpuinfo_path);
+    CreateFile("MemTotal:      123456 kB\nMemFree:        7890 kB\n", config_.meminfo_path);
+    mkdir(config_.tmp_path.c_str(), 0777);
+    CreateFile("1234.56 789.10\n", "tmp/testTmpFile");
+  }
+
+  void TearDown() override {
+    remove("tmp/testTmpFile");
+    rmdir(config_.tmp_path.c_str());
+    remove(config_.meminfo_path.c_str());
+    remove(config_.cpuinfo_path.c_str());
+    remove(config_.uptime_path.c_str());
+    remove(config_.stat_path.c_str());
+    remove(config_.cgroup_path.c_str());
+  }
+
+  FunctionHostInformationCollectorConfiguration config_{"/tmp/exampleCgroupFile.txt",
+                                                        "echo \"5.6.7.8\"",
+                                                        false,
+                                                        "echo \"OSDetails\"",
+                                                        "ls",
+                                                        "tmp",
+                                                        "/tmp/exampleStatFile.txt",
+                                                        "/tmp/exampleUptimeFile",
+                                                        "/tmp/exampleCpuinfoFile",
+                                                        "/tmp/exampleMeminfoFile",
+                                                        true};
 };
 
 TEST_F(FunctionHostInformationTest, FunctionHostInformationTestIdentificationWithoutPublicIp) {
-  set_up();
-  EXPECT_FALSE(config.collect_ip_public);
-  config.ip_public_command = "exit 1";
+  EXPECT_FALSE(config_.collect_ip_public);
+  config_.ip_public_command = "exit 1";
 
-  FunctionHostInformationCollector collector(config);
+  FunctionHostInformationCollector collector(config_);
   FunctionHostInformationIdentification information_identification = collector.CollectInformationIdentification();
   EXPECT_EQ(information_identification.id, "pQEzKi");
   // We don't know the real private ip address, but at least, it should not be empty.
   EXPECT_NE(information_identification.ip_private, "");
   EXPECT_EQ(information_identification.ip_public, "");
-
-  tear_down();
 }
 
 TEST_F(FunctionHostInformationTest, FunctionHostInformationTestIdentificationWithPublicIp) {
-  set_up();
-  config.collect_ip_public = true;
+  config_.collect_ip_public = true;
 
-  FunctionHostInformationCollector collector(config);
+  FunctionHostInformationCollector collector(config_);
   FunctionHostInformationIdentification information_identification = collector.CollectInformationIdentification();
   EXPECT_EQ(information_identification.ip_public, "5.6.7.8");
-
-  tear_down();
 }
 
 TEST_F(FunctionHostInformationTest, FunctionHostInformationTestEnvironment) {
-  set_up();
-
-  FunctionHostInformationCollector collector(config);
+  FunctionHostInformationCollector collector(config_);
   FunctionHostInformationEnvironment information_environment = collector.CollectInformationEnvironment();
   EXPECT_EQ(information_environment.operating_system_details, "OSDetails");
   EXPECT_EQ(information_environment.file_system_details, "testTmpFile");
   EXPECT_EQ(information_environment.boot_time_seconds, 123456);
   EXPECT_EQ(information_environment.uptime_seconds, 1234);
-
-  tear_down();
 }
 
 TEST_F(FunctionHostInformationTest, FunctionHostInformationTestResources) {
-  set_up();
-
-  FunctionHostInformationCollector collector(config);
+  FunctionHostInformationCollector collector(config_);
   FunctionHostInformationResources information_resources = collector.CollectInformationResources();
   EXPECT_EQ(information_resources.cpu_count, 2);
   EXPECT_EQ(information_resources.cpu_model, "CpuModelName");
   EXPECT_EQ(information_resources.cpu_features, "three test flags");
   EXPECT_EQ(information_resources.ram_size_mb, 120);
-
-  tear_down();
 }
 
 TEST_F(FunctionHostInformationTest, FunctionHostInformationTestJson) {
-  set_up();
-
   const auto* expected_json = R"""({
 	"identification":	{
 		"id":	"pQEzKi",
@@ -126,7 +109,7 @@ TEST_F(FunctionHostInformationTest, FunctionHostInformationTestJson) {
 	}
 })""";
 
-  FunctionHostInformationCollector collector(config);
+  FunctionHostInformationCollector collector(config_);
   const std::string collected_json = collector.CollectJson();
   ASSERT_TRUE(collected_json.find("pQEzKi") != collected_json.npos);
 
@@ -137,8 +120,6 @@ TEST_F(FunctionHostInformationTest, FunctionHostInformationTestJson) {
                                                                           information_identification.ip_public};
   auto json = collector.AsJson(mocked_information_identification, information_environment, information_resources);
   EXPECT_EQ(json, expected_json);
-
-  tear_down();
 }
 
 }  // namespace skyrise
