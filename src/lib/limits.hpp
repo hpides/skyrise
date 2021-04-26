ca8626163f30e7aa8340a30782cdde65759e0157
d-justen <33455240+d-justen@users.noreply.github.com>
2021-04-26 09:31:39 +0200

    Decrease Lambda Timeout (#427)

--- src/lib/limits.hpp
+++ src/lib/limits.hpp
@@ -2,7 +2,12 @@
 
 namespace skyrise {
 
-// cf. https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-limits.html
-const size_t kLambdaFunctionTimeoutSeconds = 900;
+/**
+ * The AWS Lambda service-side timeout limit for functions is 15 minutes (cf.
+ * https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-limits.html). Our own timeout is reduced to 5 minutes,
+ * because we expect our cloud functions to never run longer than that. Thereby, we keep costs for erroneous function
+ * invocations that never terminate in check.
+ */
+const size_t kLambdaFunctionTimeoutSeconds = 300;
 
 }  // namespace skyrise
