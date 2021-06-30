83568ed5bea3f02b68c1baef9228e827bf1ac258
d-justen <33455240+d-justen@users.noreply.github.com>
2021-06-30 12:02:31 +0200

    TableWrapper (#535)
    Introduces TableWrapper, an operator wrapping a table to allow pipelines without file import

---
+++ src/test/lib/operator/table_wrapper.hpp
@@ -0,0 +1,27 @@
+/**
+ * Taken and modified from our sister project Hyrise (https://github.com/hyrise/hyrise)
+ */
+#pragma once
+
+#include "operator/abstract_operator.hpp"
+
+namespace skyrise {
+
+/**
+ * Operator that wraps a table.
+ */
+class TableWrapper : public AbstractOperator {
+ public:
+  explicit TableWrapper(std::shared_ptr<const Table> table)
+      : AbstractOperator(OperatorType::kImport), table_(std::move(table)) {}
+
+  const std::string& Name() const override { return kOperatorName; }
+
+ protected:
+  std::shared_ptr<const Table> OnExecute() override { return table_; }
+
+  const std::shared_ptr<const Table> table_;
+  const std::string kOperatorName{"TableWrapper"};
+};
+
+}  // namespace skyrise
