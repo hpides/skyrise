38a190481d2bdad281c82f955a7f4e7bc9c116e0
Julian Menzler <julianmenzler@gmail.com>
2022-07-04 18:02:17 +0200

    Add file for Pipeline Slicer tests

---
+++ src/test/lib/compiler/physical_query_plan/pqp_pipeline_slicer_test.cpp
@@ -0,0 +1,28 @@
+#include "compiler/physical_query_plan/pqp_pipeline_slicer.hpp"
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "expression/expression_functional.hpp"
+
+namespace skyrise {
+
+using namespace skyrise::expression_functional;  // NOLINT(google-build-using-namespace)
+
+class PqpPipelineSlicerTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    return;
+  }
+
+ protected:
+};
+
+TEST_F(PqpPipelineSlicerTest, Test) {
+  return;
+}
+
+}  // namespace skyrise
