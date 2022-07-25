d4c3ec71bf10c05ed71065b9edff452ab15ae635
Julian Menzler <julianmenzler@gmail.com>
2022-07-25 14:14:39 +0200

    Improve helpers

--- src/test/lib/compiler/physical_query_plan/pqp_test_utils.cpp
+++ src/test/lib/compiler/physical_query_plan/pqp_test_utils.cpp
@@ -4,6 +4,45 @@
 
 namespace skyrise {
 
+namespace {
+
+/**
+ * @return the according TpchTable, based on the prefix of @param tpch_column_name.
+ */
+std::unordered_map<std::string, ColumnID> ColumnIdsByColumnNames(const TableColumnDefinitions column_definitions) {
+  std::unordered_map<std::string, ColumnID> column_id_by_column_name;
+  column_id_by_column_name.reserve(column_definitions.size());
+  for (ColumnID i = 0; i < column_definitions.size(); ++i) {
+    column_id_by_column_name.emplace(column_definitions[i].name, i);
+  }
+  return column_id_by_column_name;
+}
+
+/**
+ * @return the according TpchTable, based on the prefix of @param tpch_column_name.
+ */
+TpchTable ResolveTpchTable(const std::string& tpch_column_name) {
+  switch (tpch_column_name.at(0)) {
+    case 'c':
+      return TpchTable::kCustomer;
+    case 'l':
+      return TpchTable::kLineItem;
+    case 'o':
+      return TpchTable::kOrders;
+    case 'p':
+      if (tpch_column_name.at(1) == 's') {
+        return TpchTable::kPartSupp;
+      }
+      return TpchTable::kPart;
+    case 's':
+      return TpchTable::kSupplier;
+    default:
+      Fail("Could not resolve TpchTable, given the column name '" + tpch_column_name + "'");
+  }
+}
+
+}  // namespace
+
 std::shared_ptr<ImportOperatorProxy> CreateMockObjectReferences(const std::string& key_prefix, size_t count) {
   std::vector<ObjectReference>& object_references;
   object_references.reserve(count);
@@ -16,27 +55,46 @@ std::shared_ptr<ImportOperatorProxy> CreateMockObjectReferences(const std::strin
   return object_references;
 }
 
-std::shared_ptr<ImportOperatorProxy> CreateTpchImportProxy(const TpchTable tpch_table,
-                                                           const std::vector<std::string> column_names,
-                                                           const std::vector<ObjectReference> object_references) {
-  const TableColumnDefinitions column_definitions = TpchColumnDefinitionsByTable(tpch_table);
+std::shared_ptr<PqpColumnExpression> TpchPqpColumn(const std::string tpch_column_name) {
+  const TpchTable tpch_table = ResolveTpchTable(column_name);
 
-  // Derive ColumnIDs from column names.
-  std::vector<ColumnID> column_ids;
-  for (const std::string& column_name : column_names) {
-    ColumnID column_id = kInvalidColumnId;
-    for (ColumnID current_column_id = 0; current_column_id < column_definitions.size(); ++current_column_id) {
-      if (column_definitions.at(current_column_id).name == column_name) {
-        column_id = current_column_id;
-        break;
-      }
+  // Resolve ColumnID
+  ColumnID column_id = kInvalidColumnID;
+  const TableColumnDefinitions column_definitions = TpchColumnDefinitionsByTable(tpch_table);
+  for (ColumnID i = 0; i < column_definitions.size(); ++i) {
+    if (column_definitions[i].name != tpch_column_name) {
+      continue;
     }
-    ASSERT_NE(column_id, kInvalidColumnId);
-    column_ids.push_back(column_id);
+    column_id = i;
+    break;
+  }
+  if (column_id == kInvalidColumnID) {
+    Fail("Could not resolve Tpch PQP column definition.");
+  }
+
+  // Create PqpColumnExpression
+  const auto column_definition = column_definitions[column_id];
+  return PqpColumn_(column_id, column_definition.data_type, column_definition.nullable, column_definition.name);
+}
+
+std::shared_ptr<ImportOperatorProxy> TpchImportProxy(const std::vector<std::string> column_names,
+                                                                const std::vector<ObjectReference> object_references) {
+  Assert(!column_names.empty(), "At least one column name must be provided for a TpchTable ImportOperatorProxy.");
+  const TpchTable tpch_table = ResolveTpchTable(column_names[0]);
+
+  const TableColumnDefinitions column_definitions = TpchColumnDefinitionsByTable(tpch_table);
+  const auto column_id_by_column_name = ColumnIdsByColumnNames(column_definitions);
+
+  // Resolve import ColumnIDs.
+  std::vector<ColumnID> import_column_ids;
+  for (const std::string& column_name : column_names) {
+    const auto column_id_by_column_name_iter = column_id_by_column_name.find(column_name);
+    Assert(column_id_by_column_name_iter != column_id_by_column_name.cend(), "Could not resolve ColumnID for column '" + column_name + "'");
+    import_column_ids.push_back(column_id);
   }
 
   // Create proxy & set TPC-H table name as a comment to support debugging.
-  const auto import_proxy = ImportOperatorProxy::Make(object_references, column_ids);
+  const auto import_proxy = ImportOperatorProxy::Make(object_references, import_column_ids);
   import_proxy->SetComment(std::string(magic_enum::enum_name(tpch_table)));
 
   return import_proxy;
--- src/test/lib/compiler/physical_query_plan/pqp_test_utils.hpp
+++ src/test/lib/compiler/physical_query_plan/pqp_test_utils.hpp
@@ -4,26 +4,25 @@
 
 #include "compiler/physical_query_plan/operator_proxy/abstract_operator_proxy.hpp"
 #include "compiler/physical_query_plan/operator_proxy/import_operator_proxy.hpp"
+#include "expression/pqp_column_expression.hpp"
 
 namespace skyrise {
 
 /**
- *
- * @param key_prefix
- * @param count
- * @return
+ * @return @param count ObjectReferences for testing purposes from @param key_prefix.
  */
 std::shared_ptr<ImportOperatorProxy> CreateMockObjectReferences(const std::string& key_prefix, size_t count);
 
 /**
- *
- * @param tpch_table
- * @param column_names
- * @param object_references
- * @return
+ * @return a TpchTable PqpColumnExpression for the given @param column_name.
  */
-std::shared_ptr<ImportOperatorProxy> CreateTpchImportProxy(const TpchTable tpch_table,
-                                                           const std::vector<std::string> column_names,
-                                                           const std::vector<ObjectReference> object_references);
+std::shared_ptr<PqpColumnExpression> TpchPqpColumn(const std::string column_name);
+
+/**
+ * Resolves TpchTable by the given @param column_names and
+ * @returns an ImportOperatorProxy with the according TpchTable import column ids and given @param object_references.
+ */
+std::shared_ptr<ImportOperatorProxy> TpchImportProxy(const std::vector<std::string> column_names,
+                                                                const std::vector<ObjectReference> object_references);
 
 }  // namespace skyrise
