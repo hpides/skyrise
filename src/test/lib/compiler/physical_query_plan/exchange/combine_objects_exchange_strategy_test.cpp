110bde7fa8fe87527b6375e04d91e0e6dc9cbe17
Julian Menzler <julianmenzler@gmail.com>
2022-08-02 15:56:29 +0200

    include?

--- src/test/lib/compiler/physical_query_plan/exchange/combine_objects_exchange_strategy_test.cpp
+++ src/test/lib/compiler/physical_query_plan/exchange/combine_objects_exchange_strategy_test.cpp
@@ -8,7 +8,7 @@
 #include "compiler/compilation_context.hpp"
 #include "configuration.hpp"
 #include "metadata/mock_catalog.hpp"
-#include "utils/pqp_test_utils.hpp"
+#include "src/test/utils/pqp_test_utils.hpp"
 
 namespace skyrise {
 
